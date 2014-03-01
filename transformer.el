42b2aad0f98ae445d43f4c9df7213e2cb6706fe5
mschuldt <mbschuldt@gmail.com>
2014-02-28 19:45:39 -0800

    cleanup

--- transformer.el
+++ transformer.el
@@ -17,100 +17,73 @@ Bind variables according to VARLIST, like `let*' "
   "make a new transform table"
   `(push (cons ,table-name (make-hash-table ,@hash-args)) transform-tables))
 
-(defun set-transform-table (table-name)
-  (setq current-transform-table (get-transform-table table-name)))
-
-
 (defun get-transform-table (table-name)
   (cdr (assoc table-name transform-tables)))
+
+(defun set-transform-table (table-name)
+  (setq current-transform-table (get-transform-table table-name)))
 
 (defun get-transform (name table-name)
   (gethash name (get-transform-table table-name)))
 
-
-(defmacro translate-form (form)
-  )
-
 ;;transform modifiers, 
 (defvar transform-no-eval nil) ;;for debugging
-(defvar transform-quote-args t);;this should be set by the transform def
+(defvar transform-quote-args t);;TODO: this should be set by the transform def
+
 
 (defun transform (&rest code) 
   (cond ((> (length code) 1)
 	 (mapconcat 'transform code "\n")) ;;TODO: optional newline
+	
 	;;special case: list whose car is a list
 	((and (= (length code) 1) 
 	      (and (consp (car code))
 		   (consp (caar code))))
 	 (cons (transform (caar code)) (mapcar 'transform (cdar code))))
-	(t
-    (let ((code (car code))
-	  var--func)
-      (when (and transform-quote-args (listp code))
-	  (setq orig-code code
-		code `(,(car code)
-		       ,@(mapcar (lambda (x) `(quote ,x)) (cdr code)))))
-      (case (type-of code)
-	((cons list) ;;TODO: what if (car code) is not of type symbol? possible?
-	 (if (and (eq (type-of (car code)) 'symbol)
-		  (setq var--func (gethash (car code) current-transform-table)))
+	
+	(t (let ((code (car code))
+		 var--func)
+	     (if (and transform-quote-args (listp code))
+		 (setq orig-code code
+		       code `(,(car code)
+			      ,@(mapcar (lambda (x) `(quote ,x)) (cdr code)))))
+	   (case (type-of code)
+	     ((cons list)
+	      (if (and (eq (type-of (car code)) 'symbol)
+		       (setq var--func (gethash (car code)
+						current-transform-table)))
+		  (let* ((varlist (car var--func))
+			 (func (cdr var--func))
+			 (args (cdr code))
+			 fcall)
+		    (when func ;;TODO: if not raise error
+		      (setq fcall `(let* ,varlist (funcall ,func ,@args)))
+		      (if transform-no-eval
+			  fcall
+			(eval fcall))))
+		;;else: no defined transform
+		(if (null (car code))
+		    nil ;;TODO: why not transform the args?
+					
+		  `(,(car code) ,@(mapcar 'transform (cdr orig-code))))));;don't use quoted args
 
-	     (let* ((varlist (car var--func))
-		    (func (cdr var--func))
-		   (args (cdr code))
-		   fcall)
-	       (when func
-		 (setq fcall `(let* ,varlist (funcall ,func ,@args)))
-		 (if transform-no-eval
-		     fcall
-		   (eval fcall))))
-	   ;;else: no defined transform
-	     (if (null (car code))
-		 nil ;;?
-	       `(,(car code) ,@(mapcar 'transform (cdr orig-code)))))) ;;don't use quoted args
+	     ((symbol integer float string) code)
 
-;;	(string (pp-to-string code));(concat "\""  (replace-regexp-in-string "\\\" "\\\\\" code) "\"")) ;;TODO: does this account for all cases?
-
-	((symbol integer float string) code)
-
-	(vector
-	 "<vector>")
-	(hash-table
-	 "<hash-table>"))))))
+	     (vector
+	      "<TODO: transform vector>")
+	     (hash-table
+	      "<TODO: transform hash-table>"))))))
 
 ;;DOES NOT WORK
 ;; (defun transform-with (table-name &rest code)
 ;;   (let ((current-transform-table (get-transform-table table-name)))
 ;;     (transform code)))
   
-		       
 (defmacro with-transform-table (table-name &rest code)
   `(let ((current-transform-table (get-transform-table ,table-name)))
      ,@code))
 
-;;WTF: when evaling buffer this would evaluate anyways!!
-;; (when nil 
-;;   (insert "hi")
-;;   ;;TODO: use the emacs testing
-;;   (setq transform-tables nil)
-;;   (make-transform-table 'test)
-;;   (get-transform-table 'test)
-;;   (def-transform test and (lambda (&rest conditions)
-;; 			    (mapconcat 'transform conditions " && ")))
-
-;;   (gethash 'and (get-transform-table 'test))
-
-;;   (set-transform-table 'test)
-
-;;   ;;test. '(func (3 4) (4 2)) => '(func (quote (3 4)) (quote (4 2)))
-;;   (let ((code '(func (3 4) (4 2))))
-;;     `(,(car code) ,@(mapcar (lambda (x) `(quote ,x)) (cdr code)))
-;;     )
- 
-;;   )
-
-;;TODO: macro stepper
+;;TODO: declare transforms for edebug
 ;;      error checker
-
 
 (provide 'transformer)
