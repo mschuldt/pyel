21091954eb24faf16c8ccbadf0c5838ad4c9fe21
mschuldt <mbschuldt@gmail.com>
2014-03-24 18:27:58 -0700

    fix

--- pyel-tests.el
+++ pyel-tests.el
@@ -711,13 +711,13 @@ except:
 
  ("[(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]" '([1 3] [1 4] [2 3] [2 1] [2 4] [3 1] [3 4]))
 
- ("matrix = [[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],]"
-  ("[[row[i] for row in matrix] for i in range(4)]"
-   '((1 5 9) (2 6 10) (3 7 11) (4 8 12))))
- 
- ("transposed = []
+ ("matrix = [[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],]
+transposed = []
 for i in range(4):
  transposed.append([row[i] for row in matrix])"
+  
+  ("[[row[i] for row in matrix] for i in range(4)]"
+   '((1 5 9) (2 6 10) (3 7 11) (4 8 12)))
   ("transposed" '((1 5 9) (2 6 10) (3 7 11) (4 8 12)))))
 
 
