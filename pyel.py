eff69faa8d66e4ba72bc902d3a3a0ea7f02167aa
mschuldt <mbschuldt@gmail.com>
2014-05-29 11:35:27 -0700

    function: back_to_open_paren

--- pyel.py
+++ pyel.py
@@ -2,3 +2,20 @@
 @interactive
 def pyel_eval_buffer():
     eval(pyel(buffer_string()))
+
+def back_to_open_paren():
+    pc = 0 #paren count: +1 for open , -1 for close
+    while not bobp() and pc != 1:
+        backward_char()
+        c = thing_at_point(quote(char))
+        if ord(c) == 34: #a bug prevents writing "\""
+            forward_char()
+            backward_sexp()
+        elif c == ')':
+            pc -= 1
+        elif c == '(':
+            pc += 1
+    if bobp() and not looking_at('('):
+        return False
+    else:
+        return True
