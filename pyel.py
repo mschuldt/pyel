0cd00d11d7c54cf64436246ce8885c85cce0c69f
mschuldt <mbschuldt@gmail.com>
2014-06-08 19:19:29 -0700

    pyel-buffer written in python

--- pyel.py
+++ pyel.py
@@ -3,6 +3,16 @@
 def pyel_eval_buffer():
     eval(pyel(buffer_string()))
 
+@interactive
+def pyel_buffer(out_buff='*pyel-output*'):
+    lisp = pyel_buffer_to_string()
+    switch_to_buffer_other_window(out_buff)
+    erase_buffer()
+    lisp_interaction_mode()
+    pyel_prettyprint(lisp)
+    goto_char(1)
+
+
 def back_to_open_paren():
     pc = 0 #paren count: +1 for open , -1 for close
     while not bobp() and pc != 1:
