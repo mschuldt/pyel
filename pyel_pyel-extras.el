b8abdbf6d0dddaadd3bd51dee39f1f214fc09bc4
mschuldt <mbschuldt@gmail.com>
2014-06-12 23:52:44 -0700

    update

--- pyel_pyel-extras.el
+++ pyel_pyel-extras.el
@@ -7,13 +7,8 @@
 (defmacro pyel--1 (l r)
   (backquote (- (\, l) (\, r))))
 
-(defmacro pyel-==5 (l r)
-  (backquote
-   (let ((__r__ (\, r))
-         (__l__ (\, l)))
-     (cond
-      ((and (numberp __l__) (numberp __r__)) (= __l__ __r__))
-      (t (equal __l__ __r__))))))
+(defmacro pyel-==1 (l r)
+  (backquote (= (\, l) (\, r))))
 
 (defmacro pyel-==6 (l r)
   (backquote
@@ -23,21 +18,11 @@
       ((and (stringp __l__) (stringp __r__)) (string= __l__ __r__))
       (t (equal __l__ __r__))))))
 
-(defmacro pyel-fcall15 (func &rest args)
-  (backquote
-   (cond
-    ((vfunction-p (\, func)) (funcall (\, func) (\,@ (pyel-sort-kwargs args))))
-    ((py-class-p (\, func))
-     (call-method (\, func) --new-- (\,@ (pyel-sort-kwargs args))))
-    ((py-instance-p (\, func))
-     (call-method (\, func) --call-- (\,@ (pyel-sort-kwargs args))))
-    (t ((\, func) (\,@ (pyel-sort-kwargs args)))))))
+(defmacro pyel-fcall8 (func &rest args)
+  (backquote ((\, func) (\,@ (pyel-sort-kwargs args)))))
 
-(defmacro pyel-not3 (x)
-  (backquote
-   (let ((__x__ (\, x)))
-     (cond ((py-object-p __x__) (--not-- __x__))
-           (t (not __x__))))))
+(defmacro pyel-not2 (x)
+  (backquote (not (\, x))))
 
 (defmacro pyel-set2 (sym val)
   (backquote (setq (\, sym) (\, val))))
