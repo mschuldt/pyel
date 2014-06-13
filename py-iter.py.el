6d922b27f001aa1724a812cd3e92f5b4ce8aee9c
mschuldt <mbschuldt@gmail.com>
2014-06-12 23:48:27 -0700

    update

--- py-iter.py.el
+++ py-iter.py.el
@@ -6,15 +6,15 @@
 
   (def --init-- (self lst) nil
        (setattr self lst lst)
-       (setattr self length (pyel-fcall15 length lst)))
+       (setattr self length (pyel-fcall8 length lst)))
 
   (def --next-- (self) nil
        (let (lst ret)
          (pyel-set2 lst (getattr self lst))
          (if lst
              (progn
-               (pyel-set2 ret (pyel-fcall15 car lst))
-               (setattr self lst (pyel-fcall15 cdr lst))
+               (pyel-set2 ret (pyel-fcall8 car lst))
+               (setattr self lst (pyel-fcall8 cdr lst))
                ret)
            (py-raise StopIteration)))))
 
@@ -23,7 +23,7 @@
 
   (def --init-- (self tup) nil
        (setattr self tup tup)
-       (setattr self length (pyel-fcall15 length tup))
+       (setattr self length (pyel-fcall8 length tup))
        (setattr self i 0))
 
   (def --next-- (self) nil
@@ -31,7 +31,7 @@
          (pyel-set2 i (getattr self i))
          (if (pyel-<31 i (getattr self length))
              (progn
-               (pyel-set2 ret (pyel-fcall15 aref (getattr self tup) i))
+               (pyel-set2 ret (pyel-fcall8 aref (getattr self tup) i))
                (setattr self i (pyel-+9 (getattr self i) 1))
                ret)
            (py-raise StopIteration)))))
@@ -41,7 +41,7 @@
 
   (def --init-- (self str) nil
        (setattr self s str)
-       (setattr self length (pyel-fcall15 length str))
+       (setattr self length (pyel-fcall8 length str))
        (setattr self i 0))
 
   (def --next-- (self) nil
@@ -50,10 +50,10 @@
          (if (pyel-<31 i (getattr self length))
              (progn
                (pyel-set2 ret
-                          (pyel-fcall15 substring
-                                        (getattr self s)
-                                        i
-                                        (pyel-+9 i 1)))
+                          (pyel-fcall8 substring
+                                       (getattr self s)
+                                       i
+                                       (pyel-+9 i 1)))
                (setattr self i (pyel-+9 (getattr self i) 1))
                ret)
            (py-raise StopIteration)))))
@@ -62,16 +62,16 @@
   nil
 
   (def --init-- (self d) nil
-       (setattr self keys (pyel-fcall15 py-dict-keys d))
-       (setattr self length (pyel-fcall15 length (getattr self keys))))
+       (setattr self keys (pyel-fcall8 py-dict-keys d))
+       (setattr self length (pyel-fcall8 length (getattr self keys))))
 
   (def --next-- (self) nil
        (let (keys ret)
          (pyel-set2 keys (getattr self keys))
          (if keys
              (progn
-               (pyel-set2 ret (pyel-fcall15 car keys))
-               (setattr self keys (pyel-fcall15 cdr keys))
+               (pyel-set2 ret (pyel-fcall8 car keys))
+               (setattr self keys (pyel-fcall8 cdr keys))
                ret)
            (py-raise StopIteration)))))
-(pyel-fcall15 provide (pyel-fcall15 quote py-iter))
+(pyel-fcall8 provide (pyel-fcall8 quote py-iter))
--- py-iter_pyel-extras.el
+++ py-iter_pyel-extras.el
@@ -19,15 +19,8 @@
       ((and (vectorp __l__) (vectorp __r__)) (pyel-vector-< __l__ __r__))
       (t (error "invalid type, expected <TODO>"))))))
 
-(defmacro pyel-fcall15 (func &rest args)
-  (backquote
-   (cond
-    ((vfunction-p (\, func)) (funcall (\, func) (\,@ (pyel-sort-kwargs args))))
-    ((py-class-p (\, func))
-     (call-method (\, func) --new-- (\,@ (pyel-sort-kwargs args))))
-    ((py-instance-p (\, func))
-     (call-method (\, func) --call-- (\,@ (pyel-sort-kwargs args))))
-    (t ((\, func) (\,@ (pyel-sort-kwargs args)))))))
+(defmacro pyel-fcall8 (func &rest args)
+  (backquote ((\, func) (\,@ (pyel-sort-kwargs args)))))
 
 (defmacro pyel-set2 (sym val)
   (backquote (setq (\, sym) (\, val))))
