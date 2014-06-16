37c822a1e309f59d4b8b1b9beacacdb715ae4012
mschuldt <mbschuldt@gmail.com>
2014-06-15 19:01:44 -0700

    list comprehension tests

--- pyel-tests.el
+++ pyel-tests.el
@@ -1059,7 +1059,18 @@ for i in range(4):
   
   ("[[row[i] for row in matrix] for i in range(4)]"
    '((1 5 9) (2 6 10) (3 7 11) (4 8 12)))
-  ("transposed" '((1 5 9) (2 6 10) (3 7 11) (4 8 12)))))
+  ("transposed" '((1 5 9) (2 6 10) (3 7 11) (4 8 12))))
+
+ ("[x for x in range(30) if x %2 == 0]"
+  '(0 2 4 6 8 10 12 14 16 18 20 22 24 26 28))
+ ("[[x,y] for x in range(5) for y in range(3)]" '((0 0) (0 1) (0 2) (1 0) (1 1) (1 2) (2 0) (2 1) (2 2) (3 0) (3 1) (3 2) (4 0) (4 1) (4 2)))
+ ("[[x,y,z] for x in range(3) for y in range(3) for z in range(2)]"
+  '((0 0 0) (0 0 1) (0 1 0) (0 1 1) (0 2 0) (0 2 1) (1 0 0) (1 0 1) (1 1 0) (1 1 1) (1 2 0) (1 2 1) (2 0 0) (2 0 1) (2 1 0) (2 1 1) (2 2 0) (2 2 1)))
+ ("[[x,y,z] for x in range(5) if x == 3 for y in range(3) for z in range(2)]"
+  '((3 0 0) (3 0 1) (3 1 0) (3 1 1) (3 2 0) (3 2 1)))
+ ("[[x,y,z] for x in range(5) for y in range(3) for z in range(2) if (x+y+z)%2 == 0]" '((0 0 0) (0 1 1) (0 2 0) (1 0 1) (1 1 0) (1 2 1) (2 0 0) (2 1 1) (2 2 0) (3 0 1) (3 1 0) (3 2 1) (4 0 0) (4 1 1) (4 2 0)))
+ ("[x+y for x in range(7) for y in range(9) if x%2==0 if y > 6]"
+  '(7 8 9 10 11 12 13 14)))
 
 
 (pyel-create-tests
